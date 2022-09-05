66adb36eb2e46710dba6d8e6fd14b98cc6e610a7
MosheOgalbo <https://github.com/MosheOgalbo>
2022-09-05 21:47:11 +0300

    added build

---
+++ client/build/precache-manifest.c180b27ebe9c37174194616d0cc1f8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6e95a822d05f6497a53",
+    "url": "/static/css/main.af1821fa.chunk.css"
+  },
+  {
+    "revision": "a6e95a822d05f6497a53",
+    "url": "/static/js/main.a6e95a82.chunk.js"
+  },
+  {
+    "revision": "ee8408d5f64d712378a8",
+    "url": "/static/js/1.ee8408d5.chunk.js"
+  },
+  {
+    "revision": "f83feee081d509ba7619",
+    "url": "/static/js/2.f83feee0.chunk.js"
+  },
+  {
+    "revision": "e83f273c929bdac0c329",
+    "url": "/static/js/runtime~main.e83f273c.js"
+  },
+  {
+    "revision": "54a32b2ece593ad14fa2902485cf7484",
+    "url": "/index.html"
+  }
+];
